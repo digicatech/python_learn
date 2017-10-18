1bac29d329d5ece7e5bd5878fc43f7a07b83d4b9
Arda Altay <aaltay@mngkargo.com.tr>
2017-10-18 11:35:07 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+#String variables
+v
+name = "John"
+surname = "Snow"
+print(name)
+print('Welcome ' + name +' '+surname)
+print (name[0]) 
+print (name[0:2]) # Substring
+print (name[2:]) # Prints name starting from 3rd character
+print(name * 3) # Prints name three times
+
+#Numeric variables
+#====================================
+#integer variable
+a = 17
+print(a)
+#floating point variable
+miles   = 1000.0
+print(miles)
+
+
+#Multiple Assignment
+k = i = 1
+print(k)
+print(i)
