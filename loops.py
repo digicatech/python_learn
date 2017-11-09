d7e4b81748af944ca92420f692ad275d0729ce79
Arda Altay <aaltay@mngkargo.com.tr>
2017-11-09 17:24:12 +0300

    loops

---
+++ loops.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+list = ["Michael","Adam","Sarah"]
+
+for name in list:
+    print(name , len(name))
+
+
+for a in range(1,15):
+    print(a)
+
+for c in "Michael":
+    print(c)
+
+for z , m in enumerate("test"):
+    print(z , m)
+
+#loop with tuple
+tup = ("a","b","c")
+for x in tup:
+    print(x)
